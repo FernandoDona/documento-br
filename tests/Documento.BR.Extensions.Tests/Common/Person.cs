7e9c1e21921ea660436f8fc66f8234379371b184
FernandoDona <fernando.fdona@gmail.com>
2024-02-13 21:23:52 -0300

    Unificando o repositório principal com o extensions

---
+++ tests/Documento.BR.Extensions.Tests/Common/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Documento.BR.Extensions.Tests.Common;
+
+public class Person
+{
+    public string? CPFString { get; set; }
+    public string? CEPString { get; set; }
+    public string? CNPJString { get; set; }
+}
